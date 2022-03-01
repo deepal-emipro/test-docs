b3e2171a194be2bcdcebf671f93f5ffb4a1b7c46
Deepal Joshi <deepal@emiprotechnologies.com>
2022-03-01 18:28:23 +0530

    Class changes

--- docs/theme/assets/js/just-the-docs.js
+++ docs/theme/assets/js/just-the-docs.js
@@ -25,9 +25,8 @@ jtd.onReady = function(ready) {
 
 function initNav() {
   jtd.addEvent(document, 'click', function(e){
-      console.log('document click');
     var target = e.target;
-    while (target && !(target.classList && target.classList.contains('has_child').parentNode.contains('li'))) {
+    while (target && !(target.classList && target.classList.contains('has_child').parentNode)) {
       console.log(target.parentNode);
       target = target.parentNode;
     }
