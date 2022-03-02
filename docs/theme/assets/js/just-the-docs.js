9b6257e39004138f8de5df8eb6ebbd681f299093
Deepal Joshi <deepal@emiprotechnologies.com>
2022-03-02 11:54:34 +0530

    Theme changes

--- docs/theme/assets/js/just-the-docs.js
+++ docs/theme/assets/js/just-the-docs.js
@@ -24,18 +24,19 @@ jtd.onReady = function(ready) {
 // Show/hide mobile menu
 
 function initNav() {
-  jtd.addEvent(document, 'click', function(e){
-    var target = e.target;
-    console.log(target.classList.contains('has_child'));
-    while (target && !(target.classList && target.classList.contains('has_child').parentNode)) {
-      console.log(target);
-      target = target.parentNode;
-    }
-    if (target) {
-      e.preventDefault();
-      target.parentNode.classList.toggle('active');
-    }
-  });
+  // jtd.addEvent(document, 'click', function(e){
+  //   var target = e.target;
+  //   console.log(e.target);
+  //   var slides = document.getElementsByClassName("has_child");
+  //   while (target && !(target.classList && target.classList.contains('has_child').parentNode)) {
+  //     console.log(target);
+  //     target = target.parentNode;
+  //   }
+  //   if (target) {
+  //     e.preventDefault();
+  //     target.parentNode.classList.toggle('active');
+  //   }
+  // });
 
   const siteNav = document.getElementById('site-nav');
   const mainHeader = document.getElementById('main-header');
