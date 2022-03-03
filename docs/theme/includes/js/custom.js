dd8ba59d3fcc05e66575ddb0f27f8e7bd4ee1da6
Deepal Joshi <deepal@emiprotechnologies.com>
2022-03-03 10:53:46 +0530

    Js changes

--- docs/theme/includes/js/custom.js
+++ docs/theme/includes/js/custom.js
@@ -14,17 +14,6 @@ setTimeout(function(){
 		$(".has_child").each(function() {
 			$(this).find('a').first().before('<a href="#" class="nav-list-expander"><svg viewBox="0 0 24 24"><use xlink:href="#svg-arrow-right"></use></svg></a>');
 		});
-		// $(document).on('click','.has_child :: before',function(){
-		// 	$(this).find('ul').toggle(
-		// 	  function() {
-		// 	  	console.log('In If');
-		// 	    $(this).css({"visibility":"hidden","display":"none"});
-		// 	  }, function() {
-		// 	  	console.log('In else');
-		// 	    $(this).css({"visibility":"visible","display":"block"});
-		// 	  }
-		// 	);
-		// });
 		if(temp_pr_version[5] == '' || temp_pr_version[5] == undefined)
 		{
 // 			window.location = $("#site-nav ul li:first a").attr('href');
