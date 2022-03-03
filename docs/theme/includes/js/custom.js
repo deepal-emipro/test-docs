0ebb4d6d9c1e4e8c993e649c3d8634862452b77a
Deepal Joshi <deepal@emiprotechnologies.com>
2022-03-03 19:14:34 +0530

    Js changes

--- docs/theme/includes/js/custom.js
+++ docs/theme/includes/js/custom.js
@@ -8,7 +8,8 @@ setTimeout(function(){
 
 setTimeout(function(){
 	$(document).ready(function() {
-		$(".site-nav").css("display","block");
+		if($( window ).width() >= 1023 )
+			$(".site-nav").css("display","block");
 		$('.site-nav ul li ul').addClass('nav-list');
 		$('.site-nav ul li ul li').addClass('nav-list-item');
 		$('.site-nav ul li ul li a').addClass('nav-list-link');		
@@ -21,8 +22,6 @@ setTimeout(function(){
 			window.location = $("#site-nav ul li:first a").attr('href');
 		}
 		$(".site-nav .nav-list-link").each(function() {
-			console.log($(this).prop("href").toString().split(temp_pr_version[4])[1]);
-			console.log(new_url[1])
 			if( $(this).prop("href").toString().split(temp_pr_version[4])[1] === new_url[1])
 			{
 				$(this).parent('li').addClass('active');
