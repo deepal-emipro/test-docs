1a475fc2d5d6136040d0b0c444beccc1ea1dcef3
Deepal Joshi <deepal@emiprotechnologies.com>
2022-03-03 18:12:50 +0530

    Js changes

--- docs/theme/includes/js/custom.js
+++ docs/theme/includes/js/custom.js
@@ -25,7 +25,7 @@ setTimeout(function(){
 			console.log(new_url[1]);
 			if( $(this).prop("href").toString().split(temp_pr_version[4])[1] === new_url[1])
 			{
-				$(this).parent('nav-list-item').addClass('active');
+				$(this).parent('li').addClass('active');
 				$(this).addClass('active');
 			}
 		});
