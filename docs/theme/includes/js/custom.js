2f5951b55c7941fb3986c35af1a1b4baa595a06d
Deepal Joshi <deepal@emiprotechnologies.com>
2022-03-16 09:56:08 +0530

    Theme changes

--- docs/theme/includes/js/custom.js
+++ docs/theme/includes/js/custom.js
@@ -31,7 +31,7 @@ setTimeout(function(){
 					$(this).parent('.has_child.nav-list-item.active').each(function() {
 					$(this).find('.nav-list li').each(function() {
 						if($(this).find('.nav-list-link').parent('li').hasClass('has_child') == false) {
-							console.log('If' + $(this).html());
+							console.log('If' + $(this).parent('.nav-list-expander').parent('.has_child.nav-list-item').html());
 							toc_data += '<li>'+
 		                			'<a href="'+ $(this).find('.nav-list-link').attr('href')+'">'+ $(this).find('.nav-list-link').html()+'</a>'+
 		              			'</li>';
